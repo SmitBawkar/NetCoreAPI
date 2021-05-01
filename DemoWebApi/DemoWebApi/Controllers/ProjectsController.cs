f09ae42cc050d41d8cd4e49f7f7e2922f6838672
Smit Bawkar <smit.bawkar@msci.com>
2021-05-01 22:36:53 +0530

    read input from query string

---
+++ DemoWebApi/DemoWebApi/Controllers/ProjectsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/api/projects/{pid}/tickets")]
+        public IActionResult GetProjectTickets(int pid, [FromQuery]int tid)
+        {
+            if(tid == 0)
+                return Ok($"Reading all tickets from project#{pid}");
+
+            return Ok($"Reading ticket#{tid} from project#{pid}");
+        }
+    }
+}
